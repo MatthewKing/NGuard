e49db5a7fcc200ab37d48e709546f427d2f3f795
Matthew King <mking@mking.net>
2012-11-08 08:56:28 +0800

    Initial prototype.

---
+++ Source/NGuard/Guard.cs
@@ -0,0 +1,52 @@
+﻿namespace NGuard
+{
+    using System;
+    using System.ComponentModel;
+
+    public static class Guard
+    {
+        public static Guard<T> Requires<T>(T value, string argumentName)
+        {
+            return new Guard<T>(value, argumentName);
+        }
+    }
+
+    public sealed class Guard<T>
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public string ArgumentName { get; private set; }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public T Value { get; private set; }
+
+        public Guard(T value, string argumentName)
+        {
+            this.Value = value;
+            this.ArgumentName = argumentName;
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public new Type GetType()
+        {
+            return base.GetType();
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Source/NGuard/GuardExtensions.cs
@@ -0,0 +1,65 @@
+﻿namespace NGuard
+{
+    using System;
+
+    public static class GuardExtensions
+    {
+        public static Guard<T> IsNotNull<T>(this Guard<T> guard) where T : class
+        {
+            if (guard.Value == null)
+            {
+                string paramName = guard.ArgumentName;
+                string message = String.Format(
+                    "{0} should not be null.",
+                    paramName);
+
+                throw new ArgumentNullException(paramName, message);
+            }
+
+            return guard;
+        }
+
+        public static Guard<string> IsNotEmpty(this Guard<string> guard)
+        {
+            if (guard.Value != null && guard.Value.Length == 0)
+            {
+                string paramName = guard.ArgumentName;
+                string message = String.Format(
+                    "{0} should not be an empty string.",
+                    paramName);
+
+                throw new ArgumentNullException(paramName, message);
+            }
+
+            return guard;
+        }
+
+        public static Guard<string> IsNotWhiteSpace(this Guard<string> guard)
+        {
+            if (guard.Value != null && guard.Value.Length > 0)
+            {
+                bool whitespace = true;
+                for (int i = 0; i < guard.Value.Length; i++)
+                {
+                    if (!Char.IsWhiteSpace(guard.Value[i]))
+                    {
+                        whitespace = false;
+                        break;
+                    }
+                }
+
+                if (whitespace)
+                {
+                    string paramName = guard.ArgumentName;
+                    string message = String.Format(
+                        "{0} should contain some non-white-space characters.",
+                        paramName);
+
+                    throw new ArgumentNullException(paramName, message);
+                }
+            }
+
+            return guard;
+        }
+    }
+}
