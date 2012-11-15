eded53c3ad65710fc6fc165dd4e9d61c0ecd5662
Matthew King <mking@mking.net>
2012-11-15 14:29:05 +0800

    XML doco and minor structural tweaks.

--- Source/NGuard/Guard.cs
+++ Source/NGuard/Guard.cs
@@ -3,50 +3,109 @@
     using System;
     using System.ComponentModel;
 
+    /// <summary>
+    /// Facilitates the instantiation of Guard&lt;T&gt; instances.
+    /// </summary>
     public static class Guard
     {
-        public static Guard<T> Requires<T>(T value, string argumentName)
+        /// <summary>
+        /// Returns a new Guard&lt;T&gt; that facilitates the validation of the specified argument.
+        /// </summary>
+        /// <typeparam name="T">The type of the argument.</typeparam>
+        /// <param name="value">The value of the argument.</param>
+        /// <param name="parameterName">The name of the parameter.</param>
+        /// <returns>
+        /// A new Guard&lt;T&gt; that facilitates the validation of the specified argument.
+        /// </returns>
+        public static Guard<T> Requires<T>(T value, string parameterName)
         {
-            return new Guard<T>(value, argumentName);
+            return new Guard<T>(value, parameterName);
         }
     }
 
+    /// <summary>
+    /// Facilitates the validation of the specified argument.
+    /// </summary>
+    /// <typeparam name="T">The type of the argument</typeparam>
     public sealed class Guard<T>
     {
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public string ArgumentName { get; private set; }
+        /// <summary>
+        /// Gets the name of the parameter.
+        /// </summary>
+        public string ParameterName { get; private set; }
 
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// <summary>
+        /// Gets the value of the argument.
+        /// </summary>
         public T Value { get; private set; }
 
-        public Guard(T value, string argumentName)
+        /// <summary>
+        /// Initializes a new instance of the Guard class.
+        /// </summary>
+        /// <param name="value">The value of the argument.</param>
+        /// <param name="parameterName">The name of the parameter.</param>
+        public Guard(T value, string parameterName)
         {
             this.Value = value;
-            this.ArgumentName = argumentName;
+            this.ParameterName = parameterName;
         }
 
+        #region System.Object methods
+
+        // We're giving each of the System.Object methods an EditorBrowsableAttribute with
+        // EditorBrowsableState.Never in order to hide them in the public API.
+
+        /// <summary>
+        /// Gets the System.Type of the current instance.
+        /// </summary>
+        /// <returns>
+        /// The exact runtime type of the current instance.
+        /// </returns>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public new Type GetType()
         {
             return base.GetType();
         }
 
+        /// <summary>
+        /// Determines whether the specified System.Object is equal to the current System.Object.
+        /// </summary>
+        /// <param name="obj">
+        /// The object to compare with the current object.
+        /// </param>
+        /// <returns>
+        /// true if the specified object is equal to the current object; otherwise, false.
+        /// </returns>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override bool Equals(object obj)
         {
             return base.Equals(obj);
         }
 
+        /// <summary>
+        /// Serves as a hash function for a particular type.
+        /// </summary>
+        /// <returns>
+        /// A hash code for the current System.Object.
+        /// </returns>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override int GetHashCode()
         {
             return base.GetHashCode();
         }
 
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>
+        /// A string that represents the current object.
+        /// </returns>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override string ToString()
         {
             return base.ToString();
         }
+
+        #endregion
     }
 }
--- Source/NGuard/GuardExtensions.cs
+++ Source/NGuard/GuardExtensions.cs
@@ -2,13 +2,22 @@
 {
     using System;
 
+    /// <summary>
+    /// Extension methods for NGuard.Guard.
+    /// </summary>
     public static class GuardExtensions
     {
+        /// <summary>
+        /// Checks that the specified value is not null. An exception is thrown otherwise.
+        /// </summary>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="guard">The guard instance that holds the value to be checked.</param>
+        /// <returns>The specified guard instance.</returns>
         public static Guard<T> IsNotNull<T>(this Guard<T> guard) where T : class
         {
             if (guard.Value == null)
             {
-                string paramName = guard.ArgumentName;
+                string paramName = guard.ParameterName;
                 string message = String.Format(
                     "{0} should not be null.",
                     paramName);
@@ -19,11 +28,17 @@
             return guard;
         }
 
+        /// <summary>
+        /// Checks that the specified value is not an empty string. An exception is thrown
+        /// otherwise.
+        /// </summary>
+        /// <param name="guard">The guard instance that holds the value to be checked.</param>
+        /// <returns>The specified guard instance.</returns>
         public static Guard<string> IsNotEmpty(this Guard<string> guard)
         {
             if (guard.Value != null && guard.Value.Length == 0)
             {
-                string paramName = guard.ArgumentName;
+                string paramName = guard.ParameterName;
                 string message = String.Format(
                     "{0} should not be an empty string.",
                     paramName);
@@ -34,6 +49,12 @@
             return guard;
         }
 
+        /// <summary>
+        /// Checks that the specified value is not comprised entirely of whitespace characters.
+        /// An exception is thrown otherwise.
+        /// </summary>
+        /// <param name="guard">The guard instance that holds the value to be checked.</param>
+        /// <returns>The specified guard instance.</returns>
         public static Guard<string> IsNotWhiteSpace(this Guard<string> guard)
         {
             if (guard.Value != null && guard.Value.Length > 0)
@@ -50,7 +71,7 @@
 
                 if (whitespace)
                 {
-                    string paramName = guard.ArgumentName;
+                    string paramName = guard.ParameterName;
                     string message = String.Format(
                         "{0} should contain some non-white-space characters.",
                         paramName);
@@ -60,6 +81,28 @@
             }
 
             return guard;
+        }
+
+        /// <summary>
+        /// Checks that the specified value is not null or an empty string. An exception is thrown
+        /// otherwise.
+        /// </summary>
+        /// <param name="guard">The guard instance that holds the value to be checked.</param>
+        /// <returns>The specified guard instance.</returns>
+        public static Guard<string> IsNotNullOrEmpty(this Guard<string> guard)
+        {
+            return guard.IsNotNull().IsNotEmpty();
+        }
+
+        /// <summary>
+        /// Checks that the specified value is not null, an empty string, or comprised entirely
+        /// of whitespace characters. An exception is thrown otherwise.
+        /// </summary>
+        /// <param name="guard">The guard instance that holds the value to be checked.</param>
+        /// <returns>The specified guard instance.</returns>
+        public static Guard<string> IsNotNullOrEmptyOrWhiteSpace(this Guard<string> guard)
+        {
+            return guard.IsNotNull().IsNotEmpty().IsNotWhiteSpace();
         }
     }
 }
