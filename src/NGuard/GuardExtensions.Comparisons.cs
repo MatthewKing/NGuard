85396269b7444ecb8d99eab4419cd92f9ac3d3a9
Matthew King <mking@mking.net>
2017-03-28 12:33:02 +0800

    Add support for nullable types in the null check and comparison guards.

--- src/NGuard/GuardExtensions.Comparisons.cs
+++ src/NGuard/GuardExtensions.Comparisons.cs
@@ -28,6 +28,28 @@ namespace NGuard
         }
 
         /// <summary>
+        /// Checks that the argument value is greater than the specified comparison value.
+        /// An exception is thrown otherwise.
+        /// </summary>
+        /// <typeparam name="T">The type of the argument.</typeparam>
+        /// <param name="guard">The guard instance that holds the argument to be checked.</param>
+        /// <param name="value">The value to compare the argument value with.</param>
+        /// <returns>The specified guard instance.</returns>
+        [DebuggerStepThrough]
+        public static Guard<T?> IsGreaterThan<T>(this Guard<T?> guard, T value)
+            where T : struct, IComparable<T>
+        {
+            if (!guard.Value.HasValue || guard.Value.Value.CompareTo(value) <= 0)
+            {
+                var paramName = guard.ParameterName;
+                var message = $"{paramName} should be greater than {value}.";
+                throw new ArgumentOutOfRangeException(paramName, message);
+            }
+
+            return guard;
+        }
+
+        /// <summary>
         /// Checks that the argument value is greater than or equal to the specified comparison
         /// value. An exception is thrown otherwise.
         /// </summary>
@@ -40,6 +62,28 @@ namespace NGuard
             where T : IComparable<T>
         {
             if (guard.Value == null || guard.Value.CompareTo(value) < 0)
+            {
+                var paramName = guard.ParameterName;
+                var message = $"{paramName} should be greater than or equal to {value}.";
+                throw new ArgumentOutOfRangeException(paramName, message);
+            }
+
+            return guard;
+        }
+
+        /// <summary>
+        /// Checks that the argument value is greater than or equal to the specified comparison
+        /// value. An exception is thrown otherwise.
+        /// </summary>
+        /// <typeparam name="T">The type of the argument.</typeparam>
+        /// <param name="guard">The guard instance that holds the argument to be checked.</param>
+        /// <param name="value">The value to compare the argument value with.</param>
+        /// <returns>The specified guard instance.</returns>
+        [DebuggerStepThrough]
+        public static Guard<T?> IsGreaterThanOrEqualTo<T>(this Guard<T?> guard, T value)
+            where T : struct, IComparable<T>
+        {
+            if (!guard.Value.HasValue || guard.Value.Value.CompareTo(value) < 0)
             {
                 var paramName = guard.ParameterName;
                 var message = $"{paramName} should be greater than or equal to {value}.";
@@ -72,6 +116,28 @@ namespace NGuard
         }
 
         /// <summary>
+        /// Checks that the argument value is less than the specified comparison value.
+        /// An exception is thrown otherwise.
+        /// </summary>
+        /// <typeparam name="T">The type of the argument.</typeparam>
+        /// <param name="guard">The guard instance that holds the argument to be checked.</param>
+        /// <param name="value">The value to compare the argument value with.</param>
+        /// <returns>The specified guard instance.</returns>
+        [DebuggerStepThrough]
+        public static Guard<T?> IsLessThan<T>(this Guard<T?> guard, T value)
+            where T : struct, IComparable<T>
+        {
+            if (!guard.Value.HasValue || guard.Value.Value.CompareTo(value) >= 0)
+            {
+                var paramName = guard.ParameterName;
+                var message = $"{paramName} should be less than {value}.";
+                throw new ArgumentOutOfRangeException(paramName, message);
+            }
+
+            return guard;
+        }
+
+        /// <summary>
         /// Checks that the argument value is less than or equal to the specified comparison
         /// value. An exception is thrown otherwise.
         /// </summary>
@@ -84,6 +150,28 @@ namespace NGuard
             where T : IComparable<T>
         {
             if (guard.Value == null || guard.Value.CompareTo(value) > 0)
+            {
+                var paramName = guard.ParameterName;
+                var message = $"{paramName} should be less than or equal to {value}.";
+                throw new ArgumentOutOfRangeException(paramName, message);
+            }
+
+            return guard;
+        }
+
+        /// <summary>
+        /// Checks that the argument value is less than or equal to the specified comparison
+        /// value. An exception is thrown otherwise.
+        /// </summary>
+        /// <typeparam name="T">The type of the argument.</typeparam>
+        /// <param name="guard">The guard instance that holds the argument to be checked.</param>
+        /// <param name="value">The value to compare the argument value with.</param>
+        /// <returns>The specified guard instance.</returns>
+        [DebuggerStepThrough]
+        public static Guard<T?> IsLessThanOrEqualTo<T>(this Guard<T?> guard, T value)
+            where T : struct, IComparable<T>
+        {
+            if (!guard.Value.HasValue || guard.Value.Value.CompareTo(value) > 0)
             {
                 var paramName = guard.ParameterName;
                 var message = $"{paramName} should be less than or equal to {value}.";
--- src/NGuard/GuardExtensions.NullChecks.cs
+++ src/NGuard/GuardExtensions.NullChecks.cs
@@ -24,5 +24,25 @@ namespace NGuard
 
             return guard;
         }
+
+        /// <summary>
+        /// Checks that the argument value is not null. An exception is thrown otherwise.
+        /// </summary>
+        /// <typeparam name="T">The type of the argument.</typeparam>
+        /// <param name="guard">The guard instance that holds the argument to be checked.</param>
+        /// <returns>The specified guard instance.</returns>
+        [DebuggerStepThrough]
+        public static Guard<T?> IsNotNull<T>(this Guard<T?> guard)
+            where T : struct
+        {
+            if (!guard.Value.HasValue)
+            {
+                var paramName = guard.ParameterName;
+                var message = $"{paramName} should not be null.";
+                throw new ArgumentNullException(paramName, message);
+            }
+
+            return guard;
+        }
     }
 }
